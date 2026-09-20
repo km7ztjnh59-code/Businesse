2ae8d564f9cff9d432ca3c67a3603fd62caec0de
km7ztjnh59-code <km7ztjnh59@privaterelay.appleid.com>
2026-09-20 16:44:40 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {
+   return {
+    base: '/Business/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
